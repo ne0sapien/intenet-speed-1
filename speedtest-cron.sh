8e9026bdfbe22e767507bdd552313e5b971a7ecf
ne0sapien <43006242+ne0sapien@users.noreply.github.com>
2020-12-31 17:30:43 +0000

    Update speedtest-cron.sh

--- speedtest-cron.sh
+++ speedtest-cron.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 #date
-date >> ISP_CPUTemp.ods
+date >> ISP_CPUTemp.txt
+#ODS is a rich text format file, so you'l want to stick to plain text
 
 #getting networkspeed in format
 /home/dwalk/speedtest-cli-extras/bin/speedtest-csv --no-share >> /home/dwalk/Logs/speedtest.log
 /home/dwalk/speedtest-cli-extras/bin/speedtest-csv --no-share >> /home/dwalk/Logs/ISP_Log.ods
 
+# Expected results
+Server ID,Sponsor,Server Name,Timestamp,Distance,Ping,Download,Upload,Share,IP Address
+24500,"Star Communications, Inc.","Clinton, NC",2020-12-31T17:24:53.566574Z,61.959562539294865,36.78,164032577.60907128,12906560.070495833,,75.184.19.46
+
+
 #CPU temp
-sudo /opt/vc/bin/vcgencmd measure_temp >> ISP_CPUTemp.ods
+sudo /opt/vc/bin/vcgencmd measure_temp >> ISP_CPUTemp.txt
 
