ca6e13d299adca25fed88b40e8d4f8403a44586b
ne0sapien <43006242+ne0sapien@users.noreply.github.com>
2021-01-01 12:32:37 +0000

    Update speedtest-cron.sh

--- speedtest-cron.sh
+++ speedtest-cron.sh
@@ -8,6 +8,11 @@ date >> ISP_CPUTemp.txt
 /home/dwalk/speedtest-cli-extras/bin/speedtest-csv --no-share >> /home/dwalk/Logs/speedtest.log
 /home/dwalk/speedtest-cli-extras/bin/speedtest-csv --no-share >> /home/dwalk/Logs/ISP_Log.ods
 
+
+#The right way to use STDOUT IN Crontab (not in a .sh file within)
+/home/dwalk/speedtest-cli-extras/bin/speedtest-csv --no-share >> /home/dwalk/Logs/speedtest.log 2>&1
+
+
 # Expected results
 Server ID,Sponsor,Server Name,Timestamp,Distance,Ping,Download,Upload,Share,IP Address
 24500,"Star Communications, Inc.","Clinton, NC",2020-12-31T17:24:53.566574Z,61.959562539294865,36.78,164032577.60907128,12906560.070495833,,75.184.19.46
